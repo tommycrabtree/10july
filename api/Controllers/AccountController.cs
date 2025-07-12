f3e4b9ab1bf1817e6ecedfcf874bd63d5614db4e
Tommy Crabtree <tommycrabtree@outlook.com>
2025-07-11 21:03:19 -0700

    End of Section 4

---
+++ api/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+using System.Security.Cryptography;
+using System.Text;
+using api.Data;
+using api.DTOs;
+using api.Entities;
+using api.Extensions;
+using api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers;
+
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")] // api/account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await EmailExists(registerDto.Email)) return BadRequest("Email can't be used, playa");
+
+        var hmac = new HMACSHA3_512();
+
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user.ToDto(tokenService);
+    }
+
+    [HttpPost("login")] // api/account/login
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+
+        if (user == null) return Unauthorized("Username no es bueno");
+
+        using var hmac = new HMACSHA3_512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password, homie");
+        }
+
+        return user.ToDto(tokenService);
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+    }
+}
--- api/Controllers/MembersController.cs
+++ api/Controllers/MembersController.cs
@@ -1,13 +1,12 @@
 using api.Data;
 using api.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers;
 
-[Route("api/[controller]")] // localhost:5001/api/members
-[ApiController]
-public class MembersController(AppDbContext context) : ControllerBase
+public class MembersController(AppDbContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<AppUser>>> GetMembers()
@@ -17,6 +16,7 @@ public class MembersController(AppDbContext context) : ControllerBase
         return members;
     }
 
+    [Authorize]
     [HttpGet("{id}")] // localhost:5001/api/members/joe-id
     public async Task<ActionResult<AppUser>> GetMember(string id)
     {
